72ec03b58035e426feb0e7c4f36153c9295f124d
sramkuma9 <ajay.rsin@gmail.com>
2021-08-23 20:35:15 +0530

    refactor - introduced a variable (mid) for readability.

--- binarySearch.go
+++ binarySearch.go
@@ -4,7 +4,7 @@ import "fmt"
 
 func main() {
 	var MyArray = []int{1, 2, 3, 4, 5, 6}
-	target := 4
+	target := 2
 	index := binarySearch(MyArray, target)
 	if index != -1 {
 		fmt.Println("The index of the target is ", index)
@@ -18,13 +18,13 @@ func binarySearch(myArray []int, target int) int {
 	startIndex := 0
 	endIndex := len(myArray) - 1
 	for startIndex <= endIndex {
-		//mid := (startIndex + endIndex) / 2
-		if target == myArray[(startIndex+endIndex)/2] {
-			return (startIndex + endIndex) / 2
-		} else if target > myArray[(startIndex+endIndex)/2] {
-			startIndex = ((startIndex + endIndex) / 2) + 1
+		mid := (startIndex + endIndex) / 2
+		if target == myArray[mid] {
+			return mid
+		} else if target > myArray[mid] {
+			startIndex = mid + 1
 		} else {
-			endIndex = ((startIndex + endIndex) / 2) - 1
+			endIndex = mid - 1
 		}
 	}
 	return -1
